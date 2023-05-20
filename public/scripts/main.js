c78e25052d265d1546ded1a494f018480f89702a
rhit-anandy <anandy@rose-hulman.edu>
2023-05-20 14:34:25 -0400

    Updated firebase functionality

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,11 +13,11 @@ var rhit = rhit || {};
 
 /** globals */
 rhit.variableName = "";
-rhit.LoginManager = null;
-rhit.HomeManager = null;
-rhit.GroupsManager = null;
-rhit.JournalManager = null;
-rhit.SoundsManager = null;
+rhit.fbLoginManager = null;
+rhit.fbHomeManager = null;
+rhit.fbGroupsManager = null;
+rhit.fbJournalEntriesManager = null;
+rhit.fbSoundsManager = null;
 
 rhit.badVariable = null;
 
@@ -46,23 +46,16 @@ rhit.JournalEntriesManager = class {
 	constructor(uid){
 		this.__uid = uid;
 		this._documentSnapshots = [];
-		this.__ref = firebase.firestore().collection('users').doc(uid).collection('journalEntries');
+		rhit.fbAuthManager.getFirebaseId()
+		.then((id) => {
+			this._ref = firebase.firestore().collection('users').doc(id).collection('journalEntries');
+		})
+		//this._ref = this._ref.doc(id).collection('journalEntries');
 	}
 
-	add(entry, rating, date){
-		
-		console.log({
-			[rhit.FB_KEY_ENTRY]: entry,
-			[rhit.FB_KEY_RATING]: rating,
-			[rhit.FB_KEY_DATE]: date,
-			[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now()
+	add(entry, rating, date){		
 
-		});
-		
-		
-		
-	
-		rhit.badVariable = this.__ref.add({
+		rhit.badVariable = this._ref.add({
 			[rhit.FB_KEY_ENTRY]: entry,
 			[rhit.FB_KEY_RATING]: rating,
 			[rhit.FB_KEY_DATE]: date,
@@ -76,9 +69,6 @@ rhit.JournalEntriesManager = class {
 			console.error("Error adding doc: ", error);
 		})
 
-		console.log("initiated adding");
-		console.log("my uid=", this.__uid);
-		console.log(rhit.badVariable);
 	}
 
 	beginListening(changeListener) {
@@ -140,10 +130,81 @@ rhit.JournalEntriesManager = class {
 	
 }
 
+rhit.AuthManager = class {
+	constructor() {
+	  this._user = null;
+	}
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			if(user){
+				let isInDataBase = false;
+				firebase.firestore().collection('users').where('email', '==', user.email).get()
+				.then((querySnapshot) =>{
+					querySnapshot.forEach((doc) => {
+						isInDataBase = true;
+					});
+					if(!isInDataBase){
+						this.addUserToDatabase();
+					}
+				})
+				console.log("signed in");
+			}
+			else
+				console.log("not signed in");
+
+			changeListener();
+		});
+	}
+	signOut() {
+		firebase.auth().signOut().catch((e) => {
+			console.log("Error");
+		});
+	}
+	addUserToDatabase(){
+		let userObject = {
+			email: this._user.email,
+			groups: []
+		};
+		firebase.firestore().collection('users').add(userObject)
+	}
+	get isSignedIn() {
+		return !!this._user;
+	}
+	get uid() {
+		return this._user.uid;
+	}
+	get displayName(){
+		return this._user.displayName;
+	}
+	get isAnonymous(){
+		return this._user.isAnonymous;
+	}
+	async getuserObject(){
+		let ob;
+		await firebase.firestore().collection('users').where('email', '==', this._user.email).get()
+		.then((querySnapshot) =>{
+			querySnapshot.forEach((doc) => {
+				 ob = doc.data();
+			});
+		})
+		return ob;
+	}
+	async getFirebaseId(){
+		let ob;
+		await firebase.firestore().collection('users').where('email', '==', this._user.email).get()
+		.then((querySnapshot) =>{
+			querySnapshot.forEach((doc) => {
+				 ob = doc.id;
+			});
+		})
+		return ob;
+	}
+}
+
 rhit.GroupsManager = class {
 	constructor(uid){
 		this._uid = uid;
-		this._documentSnapshots = [];
 		this._ref = firebase.firestore().collection('groups');
 	}
 
@@ -167,10 +228,7 @@ rhit.GroupsManager = class {
 	}
 
 	beginListening(changeListener) {
-		let query = this._ref.orderBy(rhit.FB_KEY_LAST_TOUCHED, "desc").limit(50)
-		if(this._uid){
-			query = query.where(rhit.FB_KEY_AUTHOR, "==", this.uid);
-		}
+		query = this._ref.where(rhit.FB_KEY_AUTHOR, "==", this.uid);
 		this._unsubscribe = query
 		.onSnapshot((querySnapshot) => {
 			this._documentSnapshots = querySnapshot.docs;
@@ -181,18 +239,25 @@ rhit.GroupsManager = class {
 		this._unsubscribe();
 	}
 
-	get length() {
-		return this._documentSnapshots.length;
-	}
-	getMovieQuoteAtIndex(index) {
-		const docSnapshot = this._documentSnapshots[index];
-		const mq = new rhit.MovieQuote(
-			docSnapshot.id,
-			docSnapshot.get(rhit.FB_KEY_ENTRY),
-			docSnapshot.get(rhit.FB_KEY_DATE),
-			docSnapshot.get(rhit.FB_KEY_RATING)
-		);
-		return mq;
+	async updategroupDisplay(){
+		document.querySelector('#groupButtonsContainer').innerHTML = '';
+
+		let ob = await rhit.fbAuthManager.getuserObject();
+		let groups = ob.groups;
+		for(let i = 0; i < groups.length; i++){
+			let name = '';
+			let description = '';
+			this._ref.where("name", "==", groups[i])
+			.get()
+			.then((querySnapshot) => {
+				querySnapshot.forEach((doc) => {
+					name = doc.data().name;
+					description = doc.data().description;
+					document.querySelector('#groupButtonsContainer').innerHTML += `<button class="groupButton">
+					<strong>${name}</strong><hr>${description}</button>`;
+				});
+			})
+		}
 	}
 }
 
@@ -205,24 +270,32 @@ rhit.LoginPageController = class {
 
 rhit.HomePageController = class {
 	constructor () {
-
+		document.querySelector("#signOut").onclick = (event) => {
+			rhit.fbAuthManager.signOut();
+		};
 	}
 }
 
 rhit.GroupsPageController = class {
 	constructor () {
-		
+		document.querySelector("#signOut").onclick = (event) => {
+			rhit.fbAuthManager.signOut();
+		};
+		rhit.fbGroupsManager.updategroupDisplay();
 	}
 }
 
 rhit.JournalPageController = class {
 	constructor () {
+		document.querySelector("#signOut").onclick = (event) => {
+			rhit.fbAuthManager.signOut();
+		};
 		document.querySelector("#saveButton").addEventListener("click", (event) => {
 			console.log("journal save")
 			const entryJou = document.querySelector("#journalText").value;
 			let date = new Date().toUTCString().slice(5, 16);
-			rhit.JournalEntriesManager.add(entryJou, 0, date);
-			rhit.JournalEntriesManager.createCard(entryJou,date);
+			rhit.fbJournalEntriesManager.add(entryJou, 0, date);
+			rhit.fbJournalEntriesManager.createCard(entryJou,date);
 			console.log("after add");
 			event.preventDefault();
 		});
@@ -268,32 +341,28 @@ rhit.SoundsPageController = class {
 
 rhit.initializePage = function() {
 	console.log("initializing");
-	// if (document.querySelector("#loginPage")) {
-	// 	console.log("login");
-	// 	new rhit.LoginPageController();
-		
-
-		
-	// }
-
-
 	if (document.querySelector("#loginPage")) {
-		console.log("detail");
+		console.log("login");
+		new rhit.LoginPageController();
+	}
+
+	if (document.querySelector("#homePage")) {
+		console.log("home");
         const urlParams = new URLSearchParams(window.location.search);
         new rhit.HomePageController();
     }
 
 	if (document.querySelector("#journalPage")) {
 		console.log("journal");
-		rhit.JournalEntriesManager = new rhit.JournalEntriesManager(firebase.auth().currentUser.uid);
+		rhit.fbJournalEntriesManager = new rhit.JournalEntriesManager(firebase.auth().currentUser.uid);
 		new rhit.JournalPageController();
 		
 	}
 
 	if (document.querySelector("#groupsPage")) {
 		console.log("groups");
+		rhit.fbGroupsManager = new rhit.GroupsManager();
 		new rhit.GroupsPageController();
-		
 	}
 
 	if (document.querySelector("#soundsPage")) {
@@ -303,34 +372,26 @@ rhit.initializePage = function() {
 	}
 
 };
-	
 
+rhit.checkForRedirects = function(){
+	if (document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/home.html";
+	}
+	if (!document.querySelector("#loginPage") && !rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/";
+	}
+}
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
-	firebase.auth().onAuthStateChanged(function(user) {
-		if(user) {
-			var displayName = user.displayName;
-			var email = user.email;
-			var emailVerified = user.emailVerified;
-			var photoURL = user.photoURL;
-			var isAnonymous = user.isAnonymous;
-			var uid = user.uid;
-			var providerData = user.providerData;
-			console.log("signed in");
-		} else {
-			console.log("no sign in");
-		}
+	rhit.fbAuthManager = new rhit.AuthManager();
+	rhit.fbAuthManager.beginListening(() => {
+		rhit.checkForRedirects();
 		rhit.initializePage();
 	});
-
-	
-
-	if(document.querySelector("#firebaseui-auth-container")){
-		rhit.startFirebaseUI();
-	}
-	
+	if(document.querySelector("#firebaseui-auth-container"))
+		rhit.startFirebaseUI();	
 };
 
 //FIREBASE LOGIN
